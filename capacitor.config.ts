5a3a0d8e66f5fd412aaf669f38e08ae17de90c25
vovinhd <timo.volk@gmail.com>
2022-05-09 18:33:19 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.climactivity.exhib',
+  appName: 'cy-arcade-editon',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
