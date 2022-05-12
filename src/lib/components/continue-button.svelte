a6446ef7641ce70b81690f62b661ce12f67994a7
vovinhd <timo.volk@gmail.com>
2022-05-12 21:04:06 +0200

    a very extra next button

---
+++ src/lib/components/continue-button.svelte
@@ -0,0 +1,127 @@
+<script lang="ts">
+    export let ready = false;
+</script>
+
+<div class:ready>
+    <svg
+        width="120"
+        height="100"
+        viewBox="0 0 120 100"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <g id="continue_button">
+            <path
+                id="light-1"
+                d="M58.6564 45.4205C60.614 46.5831 60.614 49.4169 58.6564 50.5795L15.0318 76.4857C13.032 77.6733 10.5 76.2321 10.5 73.9063L10.5 22.0937C10.5 19.7679 13.032 18.3267 15.0318 19.5143L58.6564 45.4205Z"
+                fill-opacity="0.6"
+            />
+            <g id="dark">
+                <path
+                    id="dark-1"
+                    d="M73.6563 45.4205C75.6139 46.5831 75.6139 49.4169 73.6563 50.5795L30.0317 76.4857C28.032 77.6733 25.4999 76.2321 25.4999 73.9063L25.4999 22.0937C25.4999 19.7679 28.032 18.3267 30.0317 19.5143L73.6563 45.4205Z"
+                    fill-opacity="0.6"
+                />
+                <path
+                    id="dark-2"
+                    d="M58.6564 45.4205C60.614 46.5831 60.614 49.4169 58.6564 50.5795L15.0318 76.4857C13.032 77.6733 10.5 76.2321 10.5 73.9063L10.5 22.0937C10.5 19.7679 13.032 18.3267 15.0318 19.5143L58.6564 45.4205Z"
+                    fill-opacity="0.8"
+                />
+            </g>
+        </g>
+    </svg>
+</div>
+
+<style lang="scss">
+    :root {
+        --dark-color: #e50046;
+        --light-color: #dc7997;
+    }
+
+    svg {
+        opacity: 0;
+
+        cursor: pointer;
+        animation: reveal 3s ease forwards;
+        animation-delay: 3s;
+
+    }
+
+    #dark {
+        fill:  var(--dark-color);
+    }
+
+    #light-1 {
+        fill: var(--light-color);
+    }
+
+    #dark-1,
+    #dark-2,
+    #light-1 {
+        animation-delay: 5s;
+
+    }
+
+    .ready {
+        @apply animate-pulse;
+    }
+
+    #dark-2 {
+        animation: dark-2 8s ease forwards;
+    }
+
+    #light-1 {
+        animation: light 8s ease forwards;
+    }
+    #dark-1 {
+        animation: dark-1 8s ease forwards;
+
+    }
+
+
+    @keyframes dark-1 {
+        from {
+            transform: translateX(20%);
+        }
+        70% {
+            transform: translateX(20%);
+        }
+        to {
+            transform: translateX(100%);
+        }
+    }
+
+    @keyframes dark-2 {
+        from {
+            transform: translateX(-100%);
+        }
+        70% {
+            transform: translateX(-100%);
+        }
+        to {
+            transform: translateX(20%);
+        }
+    }
+
+    @keyframes light {
+        from {
+            transform: translateX(20%);
+        }
+        70% {
+            transform: translateX(20%);
+        }
+        to {
+            transform: translateX(40%);
+        }
+    }
+
+    @keyframes reveal {
+        from {
+            opacity: 0;
+        }
+
+        to {
+            opacity: 1;
+        }
+    }
+</style>
--- src/routes/quiz/result/__layout.svelte
+++ src/routes/quiz/result/__layout.svelte
@@ -1,90 +1,116 @@
 <script lang="ts">
-import { goto } from '$app/navigation';
+    import { goto } from "$app/navigation";
+import ContinueButton from "$lib/components/continue-button.svelte";
 
-import { appContext } from '$lib/context';
-import { onMount } from 'svelte';
+    import { appContext } from "$lib/context";
+    import { onMount } from "svelte";
 
-    import { fly, fade } from 'svelte/transition';
+    import { fly, fade } from "svelte/transition";
 
-    let opponentReady  = true
-    let minReadingTimeOver = false
-    let timer = 0
-    let minReadingTime = 5000
-    let continueLabel = "Bereit"
+    let opponentReady = false;
+    let minReadingTimeOver = false;
+    let timer = 0;
+    let minReadingTime = 5000;
+    let continueLabel = "Bereit";
     const getSingleplayerQuizResult = () => {
-        return $appContext.selectedAnswers.find(answer => !answer.correct)
-    }
+        return $appContext.selectedAnswers.find((answer) => !answer.correct);
+    };
     const nextQuestionOrResult = () => {
-
+        if (!minReadingTimeOver) {
+            console.log("wait")
+            return
+        }
         const nextQuestionId = $appContext.quiz[$appContext.currentQuestionId];
-        console.log($appContext)
+        console.log($appContext);
         if (nextQuestionId) {
-            goto(`/quiz/question/${nextQuestionId}`)
+            goto(`/quiz/question/${nextQuestionId}`);
         } else {
             if ($appContext.singlePlayer) {
-                goto(`/gameover/${getSingleplayerQuizResult() ? 'won' : 'lost'}`)
-
+                goto(
+                    `/gameover/${getSingleplayerQuizResult() ? "won" : "lost"}`
+                );
             }
         }
-    }
+    };
 
-    let timerUpdate,timeOut
+    let timerUpdate, timeOut;
 
     const finish = () => {
         clearInterval(timerUpdate);
-        clearTimeout(timeOut)
-        timer = minReadingTime
-        minReadingTimeOver = true
-    }
-
+        clearTimeout(timeOut);
+        timer = minReadingTime;
+        minReadingTimeOver = true;
+    };
 
     onMount(() => {
-        timerUpdate = setInterval(()=> {
-            timer = timer + 16
-        }, 16)
+        timerUpdate = setInterval(() => {
+            timer = timer + 16;
+        }, 16);
 
         timeOut = setTimeout(() => {
-            finish()
-        }, minReadingTime)
+            finish();
+        }, minReadingTime);
     });
-    let currentlySelectedAnswer
+    let currentlySelectedAnswer;
     $: {
         continueLabel = opponentReady ? "Weiter" : "Bereit";
-         currentlySelectedAnswer = $appContext.currentSelectedAnswer
+        currentlySelectedAnswer = $appContext.currentSelectedAnswer;
     }
 </script>
 
-<div class="bg-zinc-50 rounded-md shadow-lg hero inset-4 p-8 content-grid" in:fly={{ x: 200, duration: 500 }} out:fly={{ x: -200, duration: 500 }}>
+<div
+    class="bg-zinc-50 rounded-md shadow-lg hero inset-4 p-8 content-grid"
+    in:fly={{ x: 200, duration: 500 }}
+    out:fly={{ x: -200, duration: 500 }}
+>
     <div>
-        <div class="{currentlySelectedAnswer?.correct ? "bg-nature-light": "bg-zinc-400"} text-white text-2xl rounded-md shadow-sm p-8 text-center selected-answer" class:correct={!!$appContext.currentSelectedAnswer?.correct}>
-            {currentlySelectedAnswer ? currentlySelectedAnswer.answerText : "keine Antwort gewählt" } {currentlySelectedAnswer?.correct ? "✔" : "❌"}
+        <div
+            class="{currentlySelectedAnswer?.correct
+                ? 'bg-nature-light'
+                : 'bg-zinc-400'} text-white text-2xl rounded-md shadow-sm p-8 text-center selected-answer"
+            class:correct={!!$appContext.currentSelectedAnswer?.correct}
+        >
+            {currentlySelectedAnswer
+                ? currentlySelectedAnswer.answerText
+                : "keine Antwort gewählt"}
+            {currentlySelectedAnswer?.correct ? "✔" : "❌"}
         </div>
-
     </div>
 
-    <div class="text-center pb-4 pt-8 px-8  text-2xl title">Hättest du's gewusst?</div>
+    <div class="text-center pb-4 pt-8 px-8  text-2xl title">
+        Hättest du's gewusst?
+    </div>
     <div class="h-full overflow-y-scroll main">
-    <slot/>
+        <slot />
     </div>
 
-    <div class="grid grid-flow-col">
-    {#if opponentReady}
-    <div class="grid content-center" in:fade>
-        <span class="p-4 rounded-full bg-zinc-200 w-96 text-center">
-            dein/e Mitspieler/in ist bereit
-        </span>
-    </div>
-    {/if}
-    <div class=" btn-cta relative">
-        <div class="absolute grid grid-flow-col h-full  shadow-sm bg-heart " style="width: { 100 * Math.min(1.0, timer/minReadingTime)}%" />
-            <!--  -->
-        <button class="absolute w-full h-full" on:click={(e) => nextQuestionOrResult()} class:btn-disabled={!minReadingTimeOver} disabled={!minReadingTimeOver}>
+    <div class="grid grid-flow-col place-content-end actions">
+        {#if opponentReady}
+            <div class="grid content-center" in:fade>
+                <span class="p-4 rounded-full bg-zinc-200 w-96 text-center">
+                    dein/e Mitspieler/in ist bereit
+                </span>
+            </div>
+        {/if}
+        <!-- <div class=" btn-cta relative ">
+            <div
+                class="absolute grid grid-flow-col h-full  shadow-sm bg-heart "
+                style="width: {100 * Math.min(1.0, timer / minReadingTime)}%"
+            />
+            <button
+                class="absolute w-full h-full"
+                on:click={(e) => nextQuestionOrResult()}
+                class:btn-disabled={!minReadingTimeOver}
+                disabled={!minReadingTimeOver}
+            >
                 {continueLabel}
+            </button>
+        </div> -->
+        <div class="cursor-pointer" on:click={(e) => nextQuestionOrResult()}>
+            <ContinueButton ready={minReadingTimeOver} />
 
-        </button>
-
+        </div>
     </div>
-</div>
 </div>
 
 <style lang="scss">
@@ -93,16 +119,15 @@ import { onMount } from 'svelte';
     }
 
     .content-grid {
-        @apply grid grid-flow-row; 
+        @apply grid grid-flow-row;
         grid-template-rows: 8rem 8rem 1fr 5rem;
-
     }
 
     .btn-cta {
-        @apply w-full overflow-hidden rounded-full text-white shadow-sm bg-heart-light overflow-clip text-clip bg-clip-content
+        @apply w-full rounded-full text-white shadow-sm bg-heart-light overflow-clip text-clip bg-clip-content;
     }
     .btn-disabled {
-        @apply text-opacity-40
+        @apply text-opacity-40;
     }
 
     .title {
@@ -114,13 +139,17 @@ import { onMount } from 'svelte';
     .selected-answer {
         opacity: 0;
         animation: fadeInUp 1s ease forwards;
-
     }
 
     .main {
         opacity: 0;
         animation: fadeInUp 1s ease forwards;
+    }
 
+    .actions {
+        opacity: 0;
+        animation: fadeInUp 1s ease forwards;
+        animation-delay: 1800ms;
 
     }
 </style>
