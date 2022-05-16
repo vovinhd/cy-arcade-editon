6f2ba7ef214b8ad341127c9b65c479a4b14d8213
vovinhd <timo.volk@gmail.com>
2022-05-16 15:55:25 +0200

    send reminders at 15:00

--- netlify/functions/mail-followup/mail-followup.ts
+++ netlify/functions/mail-followup/mail-followup.ts
@@ -97,4 +97,4 @@ const handle = async (event) => {
     }
 } 
 
-export const handler = schedule("* * * * *", (event) => handle(event));
+export const handler = schedule("0 15 * * *", (event) => handle(event));
