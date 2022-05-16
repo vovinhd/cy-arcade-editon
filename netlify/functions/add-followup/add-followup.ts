07dc00bcee86647fc278cac6effbc81bed836b8f
vovinhd <timo.volk@gmail.com>
2022-05-16 12:52:45 +0200

    added date formatting

--- netlify/functions/add-followup/add-followup.ts
+++ netlify/functions/add-followup/add-followup.ts
@@ -6,6 +6,12 @@ let {SUPABASE_URL, SUPABASE_PUBLIC_ANON_KEY, API_SECRET} = process.env
 // Provide a custom schema. Defaults to "public".
 const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLIC_ANON_KEY)
 
+const dateToStr = (date) => {
+  const offset = date.getTimezoneOffset()
+  date = new Date(date.getTime() - (offset*60*1000))
+  return date.toISOString().split('T')[0]
+}
+
 export const handler: Handler = async (event, context) => {
   const { apikey } = event.queryStringParameters
   const {email, challengeBase64} = JSON.parse(event.body)
@@ -22,7 +28,7 @@ export const handler: Handler = async (event, context) => {
   try {
     if (challengeBase64 === undefined) {
       let {data, error} = await supabase.from('follow_ups').insert({
-        email, challenge: undefined, delay: 0, remind_at: Date.now() 
+        email, challenge: undefined, delay: 0, remind_at: dateToStr(Date.now()) 
       }); 
   
       if (error) {
@@ -45,7 +51,7 @@ export const handler: Handler = async (event, context) => {
     let challengeJson = Buffer.from(challengeBase64, "base64").toString(); 
     let challengeData = JSON.parse(challengeJson);
     let {challenge, delay} = challengeData; 
-    let remind_at = Date.now() + (delay * 24* 60 *60); 
+    let remind_at = dateToStr(Date.now() + (delay * 24* 60 *60)); 
     let {data, error} = await supabase.from('follow_ups').insert({
       email, challenge, delay, remind_at
     }); 
