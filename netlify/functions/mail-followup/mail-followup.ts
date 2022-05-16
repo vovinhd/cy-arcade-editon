294f1117b98ceb2992b5db8aba2a1c25ae3ce080
vovinhd <timo.volk@gmail.com>
2022-05-16 15:51:55 +0200

    test mail sending

--- netlify/functions/mail-followup/mail-followup.ts
+++ netlify/functions/mail-followup/mail-followup.ts
@@ -1,18 +1,100 @@
 import { schedule } from '@netlify/functions';
 import { createClient } from '@supabase/supabase-js'
+import { createTransport } from 'nodemailer';
+import type { ChallengeData, FollowUp } from './types';
+import { compile} from 'handlebars'; 
+import {template} from './templates/challenge-reminder.hbs'
+let { SUPABASE_URL,
+    SUPABASE_PUBLIC_ANON_KEY,
+    NODEMAILER_SMTP,
+    NODEMAILER_PORT,
+    NODEMAILER_USESSL,
+    NODEMAILER_AUTH_USERNAME,
+    NODEMAILER_AUTH_PASSWORD,
+    NODEMAILER_FROM_ADDRESS
+} = process.env
 
-let {SUPABASE_URL, SUPABASE_PUBLIC_ANON_KEY} = process.env
+const subject = "Erinnerung an deine Challenge"
+const challengeData: {[key: string]: ChallengeData} = require("./templates/challenges.json")
+const mailMainTemplate = compile(template)
+const mailTransport = createTransport({
+    host: NODEMAILER_SMTP,
+    port: NODEMAILER_PORT,
+    secure: NODEMAILER_USESSL,
+    auth: {
+        user: NODEMAILER_AUTH_USERNAME,
+        pass: NODEMAILER_AUTH_PASSWORD
+    }
+})
 
 // Provide a custom schema. Defaults to "public".
 const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLIC_ANON_KEY)
 
-// To learn about scheduled functions and supported cron extensions, 
-// see: https://ntl.fyi/sched-func
-export const handler = schedule("@hourly", async (event) => {
-    const eventBody = JSON.parse(event.body);
-    console.log(`Next function run at ${eventBody.next_run}.`);
+const handle = async (event) => {
+    let { data, error } = await supabase.from('follow_ups').select("*").eq("reminder_sent", false)
+
+    if (error) {
+        console.error(error)
+        return {
+            statusCode: 500
+        }
+    }
+
+    let updates = 0;
+    data.map((followup: FollowUp) => {
+        let htmlToSend, textToSend = ""
+        if (followup.challenge) {
+            htmlToSend = mailMainTemplate({
+                ...challengeData[followup.challenge]
+            })
+        }
+
+        const mailOptions = {
+            from: NODEMAILER_FROM_ADDRESS,
+            to: followup.email,
+            subject,
+            html: htmlToSend,
+            text: textToSend
+        }
+
+        mailTransport.sendMail(mailOptions, async(err, res) => {
+            if (err) {
+                console.error(err)
+                return {
+                    statusCode: 500,
+                    body: JSON.stringify({
+                        message: `Mailtranport failed! ${err}` 
+                    })
+                }
+            } else {
+                followup.reminder_sent = true
+                console.log("Mail send 🎆")
+                const {data, error} = await supabase.from("follow_ups").update(followup)
+                if (error) {
+                    console.error(err)
+                    return {
+                        statusCode: 500,
+                        body: JSON.stringify({
+                            message: `Mailtranport failed! ${error}` 
+                        })
+    
+                    }    
+                } else {
+                    updates += 1
+                }
+            }
+        })
+
+    })
+
 
     return {
-        statusCode: 200
-    };
-});
+        statusCode: 200,
+        body: JSON.stringify({
+            message: `Sent ${updates} Challenge reminders!`
+        })
+
+    }
+} 
+
+export const handler = schedule("0 16 * * 0", (event) => handle(event));
---
+++ netlify/functions/mail-followup/templates/challenge-reminder.hbs.js
@@ -0,0 +1,33 @@
+export const template = `<html>
+
+<head>
+    <title>Message HTML Title</title>
+</head>
+
+<body>
+    <div>
+        <h1 style="font-weight: bold;"> Challenge Reminder </h1>
+        <p>
+            <span>{{challengeTitle}}</span>
+        </p>
+        <p>
+            <span>{{challengeText}}</span>
+        </p>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste officia ratione, ad dolor quaerat possimus
+            ipsam
+            blanditiis deserunt, ut at magni vel saepe, minus maxime voluptas aut culpa non expedita.
+        </p>
+
+        <p>
+            <span>Herzliche Grüße vom climactivity-Team</span>
+        </p>
+
+        <hr>
+        <a href="https://climactivity.de/">climactivity.de</a>
+
+    </div>
+</body>
+
+</html>`
