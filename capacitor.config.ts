d7112e22682d43f6ec17d991d2da787f0ea6f492
vovinhd <timo.volk@gmail.com>
2022-07-11 12:00:03 +0200

    config remote

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,31 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const config: CapacitorConfig = {
-  appId: 'de.climactivity.exhib',
-  appName: 'cy-arcade-editon',
-  webDir: 'build',
-  bundledWebRuntime: false,
-  ios: {}
+let config: CapacitorConfig = {
+    appId: 'de.climactivity.exhib',
+    appName: 'cy-arcade-editon',
+    webDir: 'build',
+    bundledWebRuntime: false,
+    ios: {},
 };
 
-export default config;
+let configRemote: CapacitorConfig = {
+    appId: 'de.climactivity.exhibremote',
+    appName: 'cy-arcade-remote',
+    webDir: 'build',
+    bundledWebRuntime: false,
+    ios: {},
+};
 
+if (process.env.CAP_USE_SERVER) {
+    let server = {
+        url: process.env.CAP_SERVER_ADDR,
+        cleartext: process.env.CAP_USE_CLEARTEXT === 'true',
+    };
+    config = { ...configRemote, server };
+}
+
+console.log(config);
+
+export default config;
